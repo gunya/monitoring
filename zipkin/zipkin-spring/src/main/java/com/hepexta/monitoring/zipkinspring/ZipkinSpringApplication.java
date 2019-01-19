f09d9e2b013a70d0fc7462ec86c84eaefbc276f4
gunya <hepexta@hotmail.com>
2019-01-19 23:12:33 +0300

    Tracing Plain Java application with Zipkin. Initial code

---
+++ zipkin/zipkin-spring/src/main/java/com/hepexta/monitoring/zipkinspring/ZipkinSpringApplication.java
@@ -0,0 +1,14 @@
+package com.hepexta.monitoring.zipkinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipkinSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinSpringApplication.class, args);
+    }
+
+}
+
